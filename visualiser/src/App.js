ee798ad2bd204100e583c3fe4b6e773d590c70e9
Yashwin12 <munsadwalayashwin@gmail.com>
2020-06-29 22:18:17 -0500

    Initial commit :cowboy_hat_face:

---
+++ visualiser/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../src/css/App.css';
+import LinearSearch from '../src/sortingVisualiser/LinearSearch'
+
+function App() {
+  return (
+    <div className="App">
+      <LinearSearch />      
+    </div>
+  );
+}
+
+export default App;
---
+++ visualiser/src/sortingVisualiser/LinearSearch.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class LinearSearch extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <div>
+                <span>Hello World</span>
+            </div>
+        );
+    }
+}
+
+export default LinearSearch;
