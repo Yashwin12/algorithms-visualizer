d113eb33d0250b302bfba10ca546c79c03646213
Yashwin12 <munsadwalayashwin@gmail.com>
2020-08-11 22:59:30 -0500

    Implements LandingPage + reduce it to one common css file :fire:

--- src/commonUtils/LandingPage.js
+++ src/commonUtils/LandingPage.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 class LandingPage extends Component {
   constructor(props) {
@@ -6,9 +7,44 @@ class LandingPage extends Component {
     this.state = {};
   }
 
-  // TODO: Add several navigation items over here ! 
   render() {
-    return <h1> Hello world </h1>;
+    return (
+      <div>
+        <h1> Happy Visualization! </h1>
+        <br/>
+        <table className="table table-striped">
+          <thead className = "thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Algorithm Visualiser</th>
+              <th scope="col">Algorithm Type</th>
+              <th scope="col">Average Time Complexity</th>
+              <th scope="col">Average Space Complexity</th>
+              <th scope = "col">External Reference</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td scope="row">1</td>
+              <NavLink to="/linear-search">Linear Search</NavLink>              
+              <td>Searching</td>
+              <td>O(n)</td>
+              <td>O(1)</td>
+              <a target="_blank" href= "https://www.tutorialspoint.com/data_structures_algorithms/linear_search_algorithm.htm">Linear Search</a>
+            </tr>
+
+            <tr>
+              <td scope="row">2</td>
+              <NavLink to="/binary-search">Binary Search</NavLink>
+              <td>Searching</td>
+              <td>O(n)</td>
+              <td>O(log(n))</td>
+              <a target="_blank" href= "https://www.tutorialspoint.com/data_structures_algorithms/binary_search_algorithm.htm">Binary Search</a>
+            </tr>            
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
